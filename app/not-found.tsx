71ca01be174d11010d986932207729c55b4b7d9c
Muhammad Ashar Temoor <muhammadasher183@gmail.com>
2025-07-04 23:04:26 +0500

    Feat: Input Form

--- app/not-found.tsx
+++
@@ -1,9 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="flex h-[100vh] w-full items-center justify-center">
-      <h1 className="text-2xl font-bold">404 - Page Not Found</h1>
-    </div>
-  );
-}
-// This file is used to handle 404 errors in Next.js.
-// It will be displayed when a user tries to access a page that does not exist.
